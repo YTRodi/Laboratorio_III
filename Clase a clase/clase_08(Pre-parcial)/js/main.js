1294b8e98701d5a7afedc6b1d3a5045e3b0a0d85
YTRodi <yagorodi@gmail.com>
2020-10-15 17:09:21 -0300

    feat: CRUD funcionando, falta seguir modularizando y crear las vistas.

--- Clase a clase/clase_08(Pre-parcial)/js/main.js
+++ Clase a clase/clase_08(Pre-parcial)/js/main.js
@@ -14,44 +14,131 @@ function inicializarManejadores() {
     lista = obtenerPersonas();
     proximoId = obtenerId();
 
-    divTabla = document.getElementById( 'divTabla' );
+    divTabla = document.querySelector( '#divTabla' );
     actualizarLista();
 
-    // console.log(lista)
-    // console.log(proximoId);
-
-    frmPersona = document.forms[0];
-    // console.log( document.querySelector( '#form-principal' ) );
+    frmPersona = document.querySelector( '#form-principal' );
 
     frmPersona.addEventListener( 'submit', e => {
 
-        // Tengo que preguntar quién es el emisor del evento (Pq se puede disparar un submit para el alta/baja/modif)
-        e.preventDefault(); // ABORTO EL ENVIO DEL FORMULARIO!
-
+        e.preventDefault();
 
         // --------------------- ALTA ---------------------
         const nuevaPersona = obtenerPersona();
 
         if( nuevaPersona ) {
-            
-            console.log(nuevaPersona);
 
+            // console.log(nuevaPersona);
             lista.push( nuevaPersona );
             proximoId++;
             guardarDatos();
             
-            console.log(lista);
-            console.log('El próximo id es: ' + proximoId);
+            // console.log(lista);
+            // console.log('El próximo id es: ' + proximoId);
 
             // Función 'refrescarTabla() / actualizarTabla()'.
             actualizarLista();
-
+            
             //Función ( limpiar los controles (nombre, apellido, etc etc) )
+            limpiarControles();
 
         }
 
     });
 
+    // BOTON DE BAJA 
+    const botonBaja = document.querySelector( '#btnBaja' );
+    // console.log(botonBaja);
+
+    botonBaja.addEventListener( 'click', ( e ) => {
+
+        e.preventDefault();
+        let listaLength = lista.length;
+        const idPersonaSeleccionada = parseInt( document.querySelector( '#txtId' ).value );  // Obtengo referencia del id de la persona.
+
+        if ( confirm('Seguro que desea eliminar este usuario?') ) { 
+
+            if ( confirm('Seguro seguro seguro que desea ELIMINAR este usuario?') ) { 
+ 
+                for (let i = 0; i < listaLength; i++) {
+
+                    if( lista[i].id === idPersonaSeleccionada ) {
+
+                        lista.splice( i , 1); // 1er param: indice del elemento, 2do: cuantos deseo eliminar.
+                        guardarDatos();
+                        actualizarLista();
+                        limpiarControles();
+                        break;
+                        
+                    }
+                    
+                } 
+
+            }
+
+        }
+        
+
+    });
+
+    // BOTON DE MODIFICACION 
+    const botonModificar = document.querySelector( '#btnModif' );
+
+    botonModificar.addEventListener( 'click', ( e ) => {
+
+        e.preventDefault();
+        let listaLength = lista.length;
+        const idPersonaSeleccionada = parseInt( document.querySelector( '#txtId' ).value );  // Obtengo referencia del id de la persona.
+
+        if ( confirm('Seguro que desea modificar este usuario?') ) {
+
+            for ( let i = 0; i < listaLength; i++ ) {
+
+                if( lista[i].id === idPersonaSeleccionada ) {
+    
+                    lista[i].firstName = document.querySelector( '#txtNombre' ).value
+                    lista[i].lastName = document.querySelector( '#txtApellido' ).value
+                    lista[i].email = document.querySelector( '#txtEmail' ).value
+                    lista[i].gender = document.forms[0].gender.value
+                    guardarDatos();
+                    actualizarLista();
+                    limpiarControles();
+                    alert( 'MODIFICADO CON ÉXITO!' );
+                    break;
+                    
+                }
+
+            }   
+        
+        }     
+
+             
+
+    });
+
+
+
+    // BOTON DE CANCELAR (función: agregarManejadorBtnCancelar())
+    const botonCancelar = document.querySelector( '#btnCancelar' );
+
+    botonCancelar.addEventListener( 'click' , (e) => {
+
+        // console.log('estoy tocando el boton de cancelar');
+        e.preventDefault();
+        limpiarControles();
+
+    });
+
+    // BOTON DE LIMPIAR TABLA ( Limpia el localStorage ).
+    const botonLimpiarTabla = document.querySelector( '#btnLimpiarTabla' );
+
+    botonLimpiarTabla.addEventListener( 'click', ( e ) => {
+
+        e.preventDefault();
+        localStorage.clear();
+        window.location.reload();
+
+    });
 };
 
 const obtenerId = () => {
@@ -86,7 +173,6 @@ const obtenerPersona = () => {
 const guardarDatos = () => {
 
     localStorage.setItem( 'gente', JSON.stringify( lista ) );
-    // localStorage.setItem( 'nextId', JSON.stringify( proximoId ) );
     localStorage.setItem( 'nextId', proximoId );
 
 }
@@ -105,5 +191,22 @@ const actualizarLista = () => {
         
     // }, 5000);
     
+
+};
+
+// --------------------------- MIAS ---------------------------
+// --------------------------- MIAS ---------------------------
+const limpiarControles = () => {
+
+    document.querySelector('#txtNombre').value = '';
+    document.querySelector('#txtApellido').value = '';
+    document.querySelector('#txtEmail').value = '';
+    frmPersona.gender.value = 'Male';
+    document.querySelector( '#btnAlta' ).classList.remove( 'desaparecer' ); // Le saco la clase desaparecer al botón del alta
+
+    document.querySelector( '#labelId' ).classList.add( 'desaparecer' );
+    document.querySelector( '#txtId' ).classList.add( 'desaparecer' );
+    document.querySelector( '#btnBaja' ).classList.add( 'desaparecer' );
+    document.querySelector( '#btnModif' ).classList.add( 'desaparecer' );
 
 };
--- Clase a clase/clase_08(Pre-parcial)/js/tabla.js
+++ Clase a clase/clase_08(Pre-parcial)/js/tabla.js
@@ -48,7 +48,7 @@ const crearCuerpo = ( lista ) => {
 
         }
 
-        console.log(element.id);
+        // console.log(element.id);
 
         if( element.hasOwnProperty( 'id' ) ) { // Si el elemento tiene la propiedad 'id'
 
@@ -67,7 +67,7 @@ const crearCuerpo = ( lista ) => {
         }
 
         //Le agrego el manejador del evento.
-        agregarManejadorTR( tr );
+        agregarManejadorTR( tr, lista );
 
         tBody.appendChild( tr );
 
@@ -77,33 +77,65 @@ const crearCuerpo = ( lista ) => {
 
 };
 
-const agregarManejadorTR = ( tr ) => {
+const agregarManejadorTR = ( tr, lista ) => {
+    
+    let idPersonaSeleccionada;
 
-   
+    
     // null es un falsy.
     if( tr ) { // si tr es != de null...
-
+        
         tr.addEventListener( 'click', ( e ) => {
 
-            // e.target va a ser el tr
-            // #1
-            // alert(e.target.getAttribute( 'data-id' ));
+            e.preventDefault();
 
-            // console.log(e.target);
+            idPersonaSeleccionada = parseInt( e.path[1].dataset.id );
 
-            
-            // #2
-            // console.log( e );
-            console.log( e.path[1].dataset.id ); // Me da el id.
-            // alert( e.path[1].dataset.id );
+            cargarDatosForm( lista, idPersonaSeleccionada );
+            // tr.classList.add('personaSeleccionada');
 
         });
+
 
     } else {
         console.log( 'tr null' );
     }
 
 }
+
+const cargarDatosForm = ( lista, id ) => {
+
+    const labelId = document.querySelector( '#labelId' );
+    const inputId = document.querySelector( '#txtId' );
+
+    
+    for (const persona of lista) {
+        
+        if( persona.id === id ) {
+            
+            inputId.value = persona.id;
+            document.querySelector( '#txtNombre' ).value = persona.firstName;
+            document.querySelector( '#txtApellido' ).value = persona.lastName;
+            document.querySelector( '#txtEmail' ).value = persona.email;
+            document.forms[0].gender.value = persona.gender;
+
+            // Lógica: Si toco algún tr de la tabla, el botón de alta desaparece, aparece el input del ID, el botón de baja y modificar con todos los datos de la persona seleccionada.
+            document.querySelector( '#btnAlta' ).classList.add( 'desaparecer' );
+
+            labelId.classList.remove( 'desaparecer' );
+            inputId.classList.remove( 'desaparecer' );
+
+            document.querySelector( '#btnBaja' ).classList.remove( 'desaparecer' );
+            document.querySelector( '#btnModif' ).classList.remove( 'desaparecer' );
+
+            break;
+        }
+
+    }
+
+};
+
+
 
 // ? EXPORTO POR DEFAULT!!!!
 // ? EXPORTO POR DEFAULT!!!!
